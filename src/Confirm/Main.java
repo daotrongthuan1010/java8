322b5c9d2858698a6bbe5cd239f51202466e32d8
daotrongthuan1010 <conxinloime1010@gmail.com>
2024-03-05 23:54:04 +0700

    Add config server for microservices

--- src/Confirm/Main.java
+++ src/Confirm/Main.java
@@ -1,2 +1,14 @@
-package Confirm;public class Main {
+package Confirm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Flow;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("abc");
+        list.add("bcd");
+        list.forEach(System.out::print);
+    }
 }
